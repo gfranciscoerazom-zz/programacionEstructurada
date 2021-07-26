fda958672063303b9ed2c0e36de88eb8eb2970b4
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-07-26 12:14:37 -0500

    Crecion de variable, invocacion de variable, imprimir un mensaje en pantalla y pedir inputs al usuario

--- MiPrimeraVariable/Source/main.c
+++ MiPrimeraVariable/Source/main.c
@@ -3,10 +3,42 @@
 #include <stdbool.h>
 
 int main(){
-    unsigned int integerA = 1;
+    unsigned int integerA = 134;
     float floatA = 1.1;
-    unsigned char letterA = 'A';
-    bool boolean = false;
+    double doubleA = 1.2;
+    unsigned char letterA = 'P';
+
+    int integerA1;
+    float floatA1;
+    unsigned char letterA1;
+
+    // En la siguiente línea estamos imprimiendo nuestra variable int integerA
+    printf("El valor de nuestro entero A es: %i\n", integerA);
+
+    // En la siguiente línea estamos imprimiendo nuestra variable float floatA
+    printf("El valor de nuestro float A es: %f\n", floatA);
+
+    // En la siguiente línea estamos imprimiendo nuestra variable double doubleA
+    printf("El valor de nuestro double A es: %f\n", doubleA);
+
+    // En la siguiente línea estamos imprimiendo nuestra variable char lettraA
+    printf("El valor de nuestro character A es: %c\n", letterA);
+
+    // En la siguiente línea estamos imprimiendo todas las variables en una sola línea
+    printf("La variable int es: %i, la variable float es: %f, la variable double es: %f y la variable char es: %c", integerA, floatA, doubleA, letterA);
+
+
+    printf("\nIngresa el valor de integerA1: ");
+    scanf("%i", &integerA1);
+    printf("Tu valor es: %i\n", integerA1);
+
+    printf("\nIngresa el valor de floatA1: ");
+    scanf("%f", &floatA1);
+    printf("Tu valor es: %f\n", floatA1);
+
+    printf("\nIngresa el valor de letterA1: ");
+    scanf(" %c", &letterA1);
+    printf("Tu letra es: %c\n", letterA1);
 
     return 0;
 }
