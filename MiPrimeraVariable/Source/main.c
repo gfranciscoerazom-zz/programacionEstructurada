23559c6cc01bfb9bdd275d080abfece946c3edae
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-07-26 10:38:09 -0500

    Tipos de datos en C

---
+++ MiPrimeraVariable/Source/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    unsigned int integerA = 1;
+    float floatA = 1.1;
+    unsigned char letterA = 'A';
+    bool boolean = false;
+
+    return 0;
+}
